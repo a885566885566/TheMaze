f27978308b87b8372c102a1e8f4dde3bdd2cf272
a885566885566 <a885566885566@gmail.com>
2018-12-24 19:45:22 +0800

    First commit

---
+++ Assets/Scripts/Const.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public struct Creature{
+    public float maxLive;
+    public float attackPower;
+    public float trackDistance;
+    public float attackDistance;
+    public float audioDistance;
+    public Creature(float live, float power, float track, float attack, float audio)
+    {
+        maxLive = live;
+        attackPower = power;
+        trackDistance = track;
+        attackDistance = attack;
+        audioDistance = audio;
+    }
+}
+public class Const{
+    // Input Configure
+    public const float rotateSpeed = 0.3f;
+    public const float updownSpeed = 0.3f;
+    public const float moveSpeed = 5f;
+    // Time Configure
+    public const int dayRoutine = 60;
+    public const float attackTimeInterval = 0.5f;
+    // Map Configure
+    public static Vector3 mapSize = new Vector3Int(200, 0, 200);
+    public static Vector3 mapOrigin = new Vector3Int(-100, 0, -100);
+    // Item Configure
+    public const int numItems = 7;
+    public enum GameItemID { Empty, Dirt, DirtGrass, Stone, Creeper, Slime, MainActor };
+    // Main Actor Configure
+    public const int attackPower = 10;  // MainActor Attack Power
+    public const int maxLive = 13;
+    // Enemy Configure
+    public const int numEnemy = 20;
+    public const float appearRadius = 15;
+    // Creeper
+    public static Creature Creeper = new Creature(20, 1, 30, 5, 10);
+    public static Creature Slime = new Creature(20, 1, 30, 5, 10);
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour {
+
+    public GameObject mazeProto;
+    public static bool[,] maze = new bool[(int)Const.mapSize.x, (int)Const.mapSize.z];
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
